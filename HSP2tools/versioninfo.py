664997dfcbbcdbea74576b3a69e4bef8e26194a8
paulsenne <paul.senne@respec.com>
2018-01-18 16:56:31 -0600

    Update versioninfo.py

--- HSP2tools/versioninfo.py
+++ HSP2tools/versioninfo.py
@@ -18,34 +18,30 @@ import wdmtoolbox  # modified version of Tim Cera's wdmtoolbox, BSD license, Cop
 
 def versions():
     ''' Returns the version of the Python and HSP2 library modules in a DataFrame'''
-
     packages = {
-      'HSP2': HSP2.__version__,
-      'HSP2tools': HSP2tools.__version__,
-      '  ':'',               # spacer
-      'PYTHON': sys.version.replace('\n', ''),
-      'IPYTHON': IPython.__version__,
-      ' ':'',                # spacer
+            'HSP2': HSP2.__version__,
+	    'HSP2tools': HSP2tools.__version__,
+            '  ':'',               # spacer
+            'PYTHON': sys.version.replace('\n', ''),
+            'IPYTHON': IPython.__version__,
+            ' ':'',                # spacer
 
-      'H5PY': h5py.__version__,
-      'MATPLOTLIB': matplotlib.__version__,
-      'NETWORKX': networkx.__version__,
-      'NUMBA': numba.__version__,
-      'NUMPY': numpy.__version__,
-      'PANDAS': pandas.__version__,
-      'PYTABLES': tables.__version__,
+	    'H5PY': h5py.__version__,
+            'MATPLOTLIB': matplotlib.__version__,
+            'NETWORKX': networkx.__version__,
+            'NUMBA': numba.__version__,
+            'NUMPY': numpy.__version__,
+            'PANDAS': pandas.__version__,
+            'PYTABLES': tables.__version__,
+            'WDMTOOLBOX': wdmtoolbox.__version__,  # doesn't provide version info - yet
 
-      '   ':'',               # spacer
-      'os': platform.platform(),
-      'processor': platform.processor(),
-    }
+            '   ':'',               # spacer
+            'os': platform.platform(),
+            'processor': platform.processor(),
+            }
 
     cols = ['HSP2', 'HSP2tools','  ', 'PYTHON', 'IPYTHON', ' ',
-        'H5PY', 'MATPLOTLIB','NETWORKX', 'NUMBA', 'NUMPY','PANDAS','PYTABLES',
-        '   ',  'os','processor']
-
-    if platform.system() == 'Windows':
-        dict(tuple(list(packages.items())[:13] + list({'WDMTOOLBOX': wdmtoolbox.__version__}.items())+list(packages.items())[13:]))
-        cols.insert(13,'WDMTOOLBOX')
+    'H5PY', 'MATPLOTLIB','NETWORKX', 'NUMBA', 'NUMPY','PANDAS','PYTABLES',
+    'WDMTOOLBOX', '   ',  'os','processor']
 
     return pandas.DataFrame(packages, index=['Version'], columns=cols).T
