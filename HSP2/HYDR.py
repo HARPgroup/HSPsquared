cd411f344e4157827a61a9deacb1477ca7c730ef
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2022-08-31 09:59:01 -0400

    add specl_actions to function use description

--- HSP2/HYDR.py
+++ HSP2/HYDR.py
@@ -607,9 +607,10 @@ def expand_HYDR_masslinks(flags, uci, dat, recs):
 def specl(io_manager, siminfo, uci, ts, state, specl_actions):
     ''' process special actions in this domain
 
-    CALL: specl(io_manager, siminfo, uci, ts, state)
+    CALL: specl(io_manager, siminfo, uci, ts, state, specl_actions)
        store is the Pandas/PyTable open store
        siminfo is a dictionary with simulation level infor (OP_SEQUENCE for example)
        ui is a dictionary with RID specific HSPF UCI like data
        ts is a dictionary with RID specific timeseries
-       state is a dictionary with value of ts[step - 1]'''
+       state is a dictionary with value of ts[step - 1]
+       specl_actions is a dictionary with all SPEC-ACTIONS entries'''
