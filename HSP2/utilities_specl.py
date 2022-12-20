515a8ae074e197b5967907e16fe9f8ca38d347dd
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2022-12-20 14:27:40 -0500

    1000 ops

--- HSP2/utilities_specl.py
+++ HSP2/utilities_specl.py
@@ -233,7 +233,7 @@ def load_sim_dicts(op_tokens, state_paths, state_ix, dict_ix, ts_ix):
     wd_mgd.tokenize() 
     # add a series of rando equations 
     c=["flowby", "wd_mgd", "Qintake"]
-    for k in range(100):
+    for k in range(1000):
         eqn = str(25*random.random()) + " * " + c[round((2*random.random()))]
         newq = Equation('eq' + str(k), facility, eqn)
         newq.register_path()
