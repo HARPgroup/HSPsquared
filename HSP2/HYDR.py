079ac48fea05fcefb463177129adf4cbb2743fd4
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2022-12-19 19:45:33 -0500

    Equation load all fn I know bad

--- HSP2/HYDR.py
+++ HSP2/HYDR.py
@@ -21,7 +21,7 @@ from HSP2.utilities import initm, make_numba_dict
 from HSP2.utilities_specl import *
 from HSP2.SPECL import specl, _specl_
 from HSP2.om_model_object import ModelObject
-from HSP2.om_equation import Equation, exec_eqn
+from HSP2.om_equation import *
 from HSP2.om_model_linkage import ModelLinkage, step_model_link
 import numpy as np
 from numba import int8, float32, njit, types
