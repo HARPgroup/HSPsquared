71088ce2665388e016a059f1588c44738cea6c8f
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2023-06-06 09:29:08 -0400

    info

--- HSP2/main.py
+++ HSP2/main.py
@@ -55,7 +55,7 @@ def main(io_manager:IOManager, saveall:bool=False, jupyterlab:bool=True) -> None
     specactions = uci_obj.specactions
     monthdata = uci_obj.monthdata
     specactions = {} # placeholder till added to uci parser
-    print('uci_obj.opseq.keys', uci_obj.opseq.keys())
+    print('uci_obj.opseq.INDELT_minutes', uci_obj.opseq.INDELT_minutes)
     #######################################################################################
     # initilize specactions dicts
     # need to determine the best way to do send these dicts to HYDR, a nested dictionary 
