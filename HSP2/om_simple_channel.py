692408d67c2d0c09553fb4ae510f26b57e39447b
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2023-08-02 14:44:03 -0400

    debug

--- HSP2/om_simple_channel.py
+++ HSP2/om_simple_channel.py
@@ -124,6 +124,7 @@ def step_simple_channel(op, state_ix, dict_ix):
     Qout = Qin
     S1 = state_ix[storage_ix] # initial storage from end of last time step 
     # Simple Routing
+    print("dt in minutes:", state_ix[dt_ix])
     dts = (state_ix[dt_ix] * 60)
     if (solver == 0):
         Qout = Qin - wd_mgd * 1.547 / dts + ps_mgd * 1.547 / dts
