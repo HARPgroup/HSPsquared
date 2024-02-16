ce423738d6dc5aed480f6b3c3a160b6b66735ab2
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2024-02-16 12:15:28 -0500

    stash an intermediate benchmark but the more up to date ones are in develop-specact-speed and develop-specact-speed-eq

--- tests/testcbp/HSP2results/benchmark.py
+++ tests/testcbp/HSP2results/benchmark.py
@@ -3,7 +3,7 @@ import os
 os.chdir("C:/usr/local/home/git/HSPsquared")
 from HSP2.main import *
 from HSP2.om import *
-from HSP2.om_equation import *
+#from HSP2.om_equation import *
 
 state = init_state_dicts()
 # set up info and timer
@@ -21,43 +21,87 @@ print("Init HYDR state context for domain", state['domain'])
 hydr_init_ix(state['state_ix'], state['state_paths'], state['domain'])
 # Now, assemble a test dataset
 container = False 
-model_root_object = ModelObject("")
-# set up the timer as the first element 
-timer = SimTimer('timer', model_root_object, siminfo)
+state_om_model_root_object(state, siminfo)
+model_root_object = state['model_root_object']
 
 facility = ModelObject('facility', model_root_object)
-c=["flowby", "wd_mgd", "Qintake"]
-flowby = Equation('flowby', facility, {'equation':'10.0'} )
-wd_mgd = Equation('wd_mgd', facility, {'equation':'2.5'} )
-Qintake = Equation('Qintake', facility, {'equation':'50.0'} )
 for k in range(1000):
-    eqn = str(25*random.random()) + " * " + c[round((2*random.random()))]
-    newq = Equation('eq' + str(k), facility, {'equation':eqn} )
-    eqn = 50.0*random.random()
-    newq = ModelConstant('con' + str(k), facility, eqn)
+    #eqn = str(25*random.random()) + " * " + c[round((2*random.random()))]
+    #newq = Equation('eq' + str(k), facility, {'equation':eqn} )
+    conval = 50.0*random.random()
+    newq = ModelConstant('con' + str(k), facility, conval)
     speca = SpecialAction('specl' + str(k), facility, {'OPTYP': 'RCHRES', 'RANGE1': 1, 'RANGE2':'', 'AC':'+=', 'VARI':'IVOL', 'VALUE':10.0, 'YR':'2000', 'DA':'1', 'MO':'1', 'HR':'1','MN':''})
 
-# adjust op_tokens length to insure capacity
-op_tokens = ModelObject.make_op_tokens(len(model_object_cache)
-# Parse, load and order all objects
-model_loader_recursive(state['model_data'], model_root_object)
-model_path_loader(ModelObject.model_object_cache)
-model_exec_list = []
-model_touch_list = []
-# put all objects in token form for fast runtime execution and sort according to dependency order
-print("Tokenizing models")
-model_tokenizer_recursive(model_root_object, ModelObject.model_object_cache, model_exec_list, model_touch_list )
+# do all linking and tokenizing, 2nd arg "io_manager" is False as we do not have an hdf5 here
+# set ops_data_type = Dict to test the Dict performance for state_ix
+ModelObject.ops_data_type = 'Dict' # this forces state_om_model_run_prep() to use Dict type for op_tokens and state_ix
+state_om_model_run_prep(state, False, siminfo) 
+op_tokens = state['op_tokens']
 
-model_exec_list = np.asarray(model_exec_list, dtype="i8") 
-# the resulting set of objects is returned.
-state['model_object_cache'] = ModelObject.model_object_cache
-state['op_tokens'] = ModelObject.op_tokens
-state['state_step_om'] = 'disabled'
+# run 1 time to compile all if anything is changed
+model_exec_list = state['model_exec_list']
+iterate_models(model_exec_list, op_tokens, state_ix, dict_ix, ts_ix, 1, -1)
 
-# using only these runnables cuts runtime by over 40%
-# Test and time the run
+# test with np.array state_ix
+#np_state_ix = np.asarray(list(state_ix.values()), dtype="float32")
+np_state_ix = zeros(max(state_ix.keys()) + 1, dtype="float32")
+# this insures that the keys in the nparray version of state match 
+for ix, iv in state_ix.items():
+    np_state_ix[ix] = iv
+
+
+# Test and time the run with Dict version of state_ix
 start = time.time()
-iterate_perf(model_exec_list, op_tokens, state_ix, dict_ix, ts_ix, siminfo['steps'], -1)
+iterate_models(model_exec_list, op_tokens, state_ix, dict_ix, ts_ix, siminfo['steps'], -1)
 end = time.time()
-print(len(model_exec_list), "components iterated over", siminfo['steps'], "time steps took" , end - start, "seconds")
+print(len(model_exec_list), "state_ix components iterated with full execution via iterate_models()", siminfo['steps'], "time steps took" , end - start, "seconds")
 
+start = time.time()
+iterate_models(model_exec_list, op_tokens, np_state_ix, dict_ix, ts_ix, siminfo['steps'], -1)
+end = time.time()
+print(len(model_exec_list), "np_state_ix components iterated with full execution via iterate_models()", siminfo['steps'], "time steps took" , end - start, "seconds")
+
+@njit
+def test_set(op, state_ix):
+    ix = op[1]
+    getsx = state_ix[ix] 
+    state_ix[ix] = getsx * 1.0 
+
+@njit
+def iteration_test_dat(op_order, it_ops, state_ix, it_nums):
+    ctr = 0
+    ttr = 0.0
+    for n in range(it_nums):
+        for i in op_order:
+            op = it_ops[i]
+            #getsx = state_ix[i] 
+            #state_ix[i] = getsx * 1.0 
+            #ttr = ttr + getsx
+            test_set(op, state_ix)
+            ctr = ctr + 1
+    print("Completed ", ctr, " loops")
+    #print("Total value", ttr)
+
+
+
+# Now test just the data structures with no actual calculation from primitives
+start = time.time()
+iteration_test_dat(model_exec_list, op_tokens, state_ix, siminfo['steps'])
+end = time.time()
+print(len(model_exec_list), "components iterated over test data setter state_ix", siminfo['steps'], "time steps took" , end - start, "seconds")
+
+start = time.time()
+iteration_test_dat(model_exec_list, op_tokens, np_state_ix, siminfo['steps'])
+end = time.time()
+print(len(model_exec_list), "components iterated over test data setter np_state_ix", siminfo['steps'], "time steps took" , end - start, "seconds")
+
+start = time.time()
+iterate_perf(model_exec_list, op_tokens, state_ix, dict_ix, ts_ix, siminfo['steps'])
+end = time.time()
+print(len(model_exec_list), "state_ix components iterated over iterate_perf (selective component) ", siminfo['steps'], "time steps took" , end - start, "seconds")
+
+
+start = time.time()
+iterate_perf(model_exec_list, op_tokens, np_state_ix, dict_ix, ts_ix, siminfo['steps'])
+end = time.time()
+print(len(model_exec_list), "np_state_ix components iterated over iterate_perf (selective component)", siminfo['steps'], "time steps took" , end - start, "seconds")
