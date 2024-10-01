08c94fc4e5e9fe26e03a8047ec4c69a7a0c9f775
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2024-10-01 10:04:51 -0400

    ruff all files in question.

--- src/hsp2/hsp2/state_fn_defaults.py
+++ src/hsp2/hsp2/state_fn_defaults.py
@@ -1,5 +1,6 @@
 # null function to be loaded when not supplied by user
-from numba import njit # import the types
+from numba import njit  # import the types
+
 
 @njit
 def state_step_hydr(state_info, state_paths, state_ix, dict_ix, ts_ix, hydr_ix, step):
