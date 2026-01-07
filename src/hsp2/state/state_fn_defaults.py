60bc530ca3f6dea7089345e67c000e046819755c
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-01-07 09:40:03 -0500

    try different name

--- src/hsp2/state/state_fn_defaults.py
+++ src/hsp2/state/state_fn_defaults.py
@@ -3,16 +3,16 @@ from numba import njit  # import the types
 from numba.typed import Dict
 from numba import types  # import the types
 
-state = {}  # shared state Dictionary, contains numba-ready Dicts
-state["state_paths"] = Dict.empty(
+state_empty = {}  # shared state Dictionary, contains numba-ready Dicts
+state_empty["state_paths"] = Dict.empty(
     key_type=types.unicode_type, value_type=types.int64
 )
-state["state_ix"] = Dict.empty(key_type=types.int64, value_type=types.float64)
-state["dict_ix"] = Dict.empty(key_type=types.int64, value_type=types.float64[:, :])
-state["ts_ix"] = Dict.empty(key_type=types.int64, value_type=types.float64[:])
+state_empty["state_ix"] = Dict.empty(key_type=types.int64, value_type=types.float64)
+state_empty["dict_ix"] = Dict.empty(key_type=types.int64, value_type=types.float64[:, :])
+state_empty["ts_ix"] = Dict.empty(key_type=types.int64, value_type=types.float64[:])
 # initialize state for hydr
 # add a generic place to stash model_data for dynamic components
-state["model_data"] = {}
+state_empty["model_data"] = {}
 
 @njit
 def state_step_hydr(state_info, state_paths, state_ix, dict_ix, ts_ix, hydr_ix, step):
