7a748eb7a19461671073ec448df4ac1a82550b40
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2026-02-02 13:37:24 -0500

    complete mapping class

--- src/hsp2/hsp2tools/HDF5.py
+++ src/hsp2/hsp2tools/HDF5.py
@@ -12,14 +12,54 @@ from hsp2 import hsp2tools
 
 
 class HDF5:
-    REQUIRES_MAPPING = ["GQUAL", "CONS", "IQUAL", "PQUAL"]
 
     def __init__(self, file_name: str) -> None:
         self.file_name = file_name
         self.aliases = hsp2_hspf_aliases()
         self.data = {}
         self.lock = Lock()
+    
+    def get_time_series(
+        self, operation: str, id: str, constituent: str, activity: str
+    ) -> Union[pd.Series, None]:
+        """Reads timeseries from HDF5 are returns the desired."""
+        operation = operation.upper()
+        constituent = constituent.upper()
+        activity = activity.upper()
+        (constituent, constituent_prefix) = self.aliases.get_constituent_prefix(activity, constituent)
+        key = (operation, id, activity)
+        try:
+            self.lock.acquire()
+            if key not in self.data.keys():
+                self.data[key] = self._read_table(operation, id, activity)
+            self.lock.release()
 
+            df = self.data[key]
+            if constituent_prefix + constituent in df.columns:
+                return df[constituent_prefix + constituent]
+            else:
+                constituent_alias = self.aliases.get_alias((operation, activity, constituent))
+                return df[constituent_prefix + constituent_alias]
+        except KeyError:
+            return None
+
+    def _read_table(self, operation: str, id: str, activity: str) -> pd.DataFrame:
+        key = f"RESULTS/{operation}_{operation[0]}{id}/{activity}/table"
+
+        try:
+            with pd.HDFStore(self.file_name, "r") as store:
+                df = pd.read_hdf(store, key=key)
+                df["index"] = pd.to_datetime(df["index"], unit="ns")
+                df = df.set_index("index")
+                return df
+        except KeyError:
+            return pd.DataFrame()
+
+
+class hsp2_hspf_aliases:
+    REQUIRES_MAPPING = ["GQUAL", "CONS", "IQUAL", "PQUAL"]
+    def __init__(self) -> None:
+        self.aliases = self._read_aliases_csv()
         self.gqual_prefixes = self._read_gqual_mapping()
         self.cons_prefixes = self._read_cons_mapping()
         self.iqual_prefixes = self._read_iqual_mapping()
@@ -50,24 +90,24 @@ class HDF5:
 
     def _read_gqual_mapping(self) -> Dict[str, str]:
         return self._read_nqual_mapping(R"RCHRES/GQUAL/GQUAL", "GQID", 7)
-
+    
     def _read_cons_mapping(self) -> Dict[str, str]:
         return self._read_nqual_mapping(R"RCHRES/CONS/CONS", "CONID", 7)
-
+    
     def _read_iqual_mapping(self) -> Dict[str, str]:
         return self._read_nqual_mapping(R"IMPLND/IQUAL/IQUAL", "QUALID", 10)
-
+    
     def _read_pqual_mapping(self) -> Dict[str, str]:
         return self._read_nqual_mapping(R"PERLND/PQUAL/PQUAL", "QUALID", 10)
-
-    def get_time_series(
-        self, operation: str, id: str, constituent: str, activity: str
-    ) -> Union[pd.Series, None]:
-        """Reads timeseries from HDF5 are returns the desired."""
-        operation = operation.upper()
-        constituent = constituent.upper()
-        activity = activity.upper()
-
+    
+    def _read_aliases_csv(self) -> Dict[Tuple[str, str, str], str]:
+        datapath = os.path.join(hsp2tools.__path__[0], "data", "HBNAliases.csv")
+        df = pd.read_csv(datapath)
+        df = df.set_index(["operation", "activity", "hspf_name"])
+        df_dict = df["hsp2_name"].to_dict()
+        return df_dict
+    
+    def get_constituent_prefix(self, activity, constituent):
         constituent_prefix = ""
         if activity in self.REQUIRES_MAPPING:
             constituent_prefix = ""
@@ -81,48 +121,7 @@ class HDF5:
                     if constituent.startswith(key):
                         constituent_prefix = f"{activity}{value}_"
                         constituent = constituent.replace(key, "")
-
-        key = (operation, id, activity)
-        try:
-            self.lock.acquire()
-            if key not in self.data.keys():
-                self.data[key] = self._read_table(operation, id, activity)
-            self.lock.release()
-
-            df = self.data[key]
-            if constituent_prefix + constituent in df.columns:
-                return df[constituent_prefix + constituent]
-            else:
-                constituent_alias = self.aliases.get_alias((operation, activity, constituent))
-                return df[constituent_prefix + constituent_alias]
-        except KeyError:
-            return None
-
-    def _read_table(self, operation: str, id: str, activity: str) -> pd.DataFrame:
-        key = f"RESULTS/{operation}_{operation[0]}{id}/{activity}/table"
-
-        try:
-            with pd.HDFStore(self.file_name, "r") as store:
-                df = pd.read_hdf(store, key=key)
-                df["index"] = pd.to_datetime(df["index"], unit="ns")
-                df = df.set_index("index")
-                return df
-        except KeyError:
-            return pd.DataFrame()
-
-
-
-class hsp2_hspf_aliases:
-    REQUIRES_MAPPING = ["GQUAL", "CONS", "IQUAL", "PQUAL"]
-    def __init__(self) -> None:
-        self.aliases = self._read_aliases_csv()
-    
-    def _read_aliases_csv(self) -> Dict[Tuple[str, str, str], str]:
-        datapath = os.path.join(hsp2tools.__path__[0], "data", "HBNAliases.csv")
-        df = pd.read_csv(datapath)
-        df = df.set_index(["operation", "activity", "hspf_name"])
-        df_dict = df["hsp2_name"].to_dict()
-        return df_dict
+        return (constituent, constituent_prefix)
     
     def get_alias(self, params):
        if params in self.aliases:
